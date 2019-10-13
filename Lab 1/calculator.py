e5fddc58138a86e4e22a715a4b04f3cf1063a966
Mauro <mauroorru3@gmail.com>
2019-10-13 16:16:34 +0200

    first commit

---
+++ Lab 1/calculator.py
@@ -0,0 +1,32 @@
+#calcultator.py
+
+def sum(m,n):
+	result = m
+	if n < 0:
+		for i in range(abs(n)):
+			result -= 1
+	else:
+		for i in range(n):
+			result +=1
+	return result
+
+
+
+
+
+def divide(m, n):
+	result = 0
+	negativeResult = m > 0 and n < 0 or m < 0 and m > 0
+	n = abs(n)
+	m = abs(m)
+
+	while (m-n) >= 0:
+		m -= n
+		result += 1
+
+	result = -result if negativeResult else result
+
+	return result
+
+
+
